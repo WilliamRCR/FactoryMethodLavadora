b02e6c8b19a0f40dcef98dd0646bf733a501ff85
William <williamcastanazaromero2@gmail.com>
2022-09-17 13:02:17 -0600

    Parcial 2 Progra II

---
+++ test/org/formacion/factorymethod/TestLavadora.java
@@ -0,0 +1,33 @@
+package org.formacion.factorymethod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLavadora {
+
+	@Test
+	public void test_carga_frontal() {
+		//implementar factory method
+		Lavadora lavadora = null;
+		lavadora.ponerMandos();
+		lavadora.ponerTambor();
+		
+		assertEquals("frontal", lavadora.tipoCarga);
+		assertTrue(lavadora.tieneMandos);
+		assertTrue(lavadora.tieneTambor);
+	}
+
+	@Test
+	public void test_carga_superior() {
+		//implementar factory method
+		Lavadora lavadora = null;
+		lavadora.ponerMandos();
+		lavadora.ponerTambor();
+		
+		assertEquals("superior", lavadora.tipoCarga);
+		assertTrue(lavadora.tieneMandos);
+		assertTrue(lavadora.tieneTambor);
+	}
+
+}
